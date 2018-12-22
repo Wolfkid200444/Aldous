6657182a375ec5b45def29bdb65873f8ab89198a
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-12-23 05:19:08 +0800

    Add files via upload

--- tests/lint.sh
+++ tests/lint.sh
@@ -3,21 +3,20 @@
 PHP_BINARY="php"
 
 while getopts "p:" OPTION 2> /dev/null; do
-  case ${OPTION} in
-  p)
-  PHP_BINARY="$OPTARG"
-  ;;
-  esac
+	case ${OPTION} in
+		p)
+			PHP_BINARY="$OPTARG"
+			;;
+	esac
 done
 
-echo Starting to running Aldous's PHP lint scans...
+echo Running PHP lint scans...
 
 OUTPUT=`find ./src/pocketmine -name "*.php" -print0 | xargs -0 -n1 -P4 "$PHP_BINARY" -l`
 
 if [ $? -ne 0 ]; then
-  echo $OUTPUT | grep -v "No syntax errors detected!"
-  exit 1
+	echo $OUTPUT | grep -v "No syntax errors"
+	exit 1
 fi
 
-echo The lint scans has been completed.
-echo All files have no syntax errors!
+echo Lint scan completed successfully.
