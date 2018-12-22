15450ff66e837fd2cbde5e3f99e372529b61f1af
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-12-22 22:09:01 +0000

    Not fair.

--- tests/travis.sh
+++ tests/travis.sh
@@ -26,9 +26,8 @@ mkdir -p ./plugins
 
 echo -e "version\nmakeserver\nstop\n\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-installer --disable-ansi --disable-readline --debug.level=2  --settings.async-workers="$PM_WORKERS" --settings.enable-dev-builds=1
 if ls plugins/Aldous/Aldous.phar >/dev/null 2>&1; then
-    echo Successfully created Aldous | Server Software PHAR file.
-    exit 0
+    echo Successfully created Aldous - Server Software PHAR file.
 else
-    echo Creating Aldous | Server Software PHAR file is unsuccessful.
+    echo Creating Aldous - Server Software PHAR file is unsuccessful.
     exit 1
 fi
