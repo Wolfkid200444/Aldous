223f7d60097134a3d7b8d9511069962206572052
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-12-22 21:32:15 +0000

    Update travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -25,9 +25,9 @@ rm server.log 2> /dev/null
 mkdir -p ./plugins
 
 echo -e "\nversion\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-installer --disable-ansi --disable-readline --debug.level=2  --settings.async-workers="$PM_WORKERS" --settings.enable-dev-builds=1
-if ls plugins/Aldous/Aldous.phar >/dev/null 2>&1; then
-    echo Successfully created Aldous | Server Software file!
+if ls plugins/Aldous/Aldous*.phar >/dev/null 2>&1; then
+    echo Successfully created Aldous | Server Software PHAR file!
 else
-    echo Creating Aldous | Server Software file is unsuccessful!
+    echo Creating Aldous | Server Software PHAR file is unsuccessful!
     exit 1
 fi
